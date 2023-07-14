cf4999fd1f362d261c912bd2945e963630a7673d
alwlsTm <mijine0716@naver.com>
2023-07-14 20:59:37 +0900

    오늘의 날짜 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import NewsList from './components/NewsList';
 import styles from './App.module.css';
 import Nav from './components/Nav';
 
+function getToday() {
+  const today = new Date();
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+  return `${today.getMonth() + 1} / ${today.getDate()} (${week[today.getDay()]})`;
+}
+
 function App() {
   const [category, setCategory] = useState("all");  //카테고리 state
   const [keyword, setKeyword] = useState("");       //검색 키워드 state
@@ -19,6 +25,7 @@ function App() {
     <div className={styles.App}>
       <Header keyword={keyword} onChange={onChange} onSubmit={onSubmit} />
       <Nav category={category} onSelect={onSelect} />
+      <div><p className={styles.date}>{getToday()}</p></div>
       <NewsList category={category} keyword={keyword} />
     </div>
   );
