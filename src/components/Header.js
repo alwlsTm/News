bef8c7beaeb521f6c112ef79d33c63091df53cb0
alwlsTm <mijine0716@naver.com>
2023-07-25 23:57:25 +0900

    us 뉴스 보완
    검색창 변경 & <select> css 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,18 @@ import LocaleSelect from './LocaleSelect';
 function Header({ keyword, locale, onChangeKeyword, onChangeLocale, onSubmit }) {
   return (
     <div className={styles.Header}>
-      <img className={styles.logo} src={logo} alt="News"></img>
-      <LocaleSelect locale={locale} onChange={onChangeLocale} />
+      <div className={styles.logoSelect}>
+        <img className={styles.logo} src={logo} alt="News"></img>
+        <div className={styles.LocaleSelect}>
+          <LocaleSelect locale={locale} onChange={onChangeLocale} />
+        </div>
+      </div>
       <form className={styles.search} onSubmit={onSubmit}>
         <input
           className={styles.searchBar}
           type="text"
           value={keyword}
-          placeholder='뉴스 검색'
+          placeholder={locale === "kr" ? '뉴스 검색' : 'Search News'}
           onChange={onChangeKeyword}>
         </input>
         <button className={styles.submit} type='submit'>
--- src/components/LocaleSelect.js
+++ src/components/LocaleSelect.js
@@ -1,6 +1,8 @@
+import styles from './LocaleSelect.module.css';
+
 function LocaleSelect({ locale, onChange }) {
   return (
-    <select value={locale} onChange={onChange}>
+    <select className={styles.LocaleSelect} value={locale} onChange={onChange}>
       <option value="kr">KR</option>
       <option value="us">US</option>
     </select>
