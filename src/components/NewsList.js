93099ae4a23d04eee8a6ad690fc794f7d5ead0c1
alwlsTm <mijine0716@naver.com>
2023-07-27 23:43:01 +0900

    URL 수정

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -39,10 +39,11 @@ function NewsList({ category, keyword, locale }) {
         `${qLocale}` +
         `${qCategory}` +
         `${qKeyword}` +
+        'pageSize=50&' +
         'apiKey=5c95bcf4e770493282e390b31b3fbb07'
       );
       setArticles(response.data.articles);
-      // console.log(response.data.articles);
+      console.log(response.data.articles);
     }
     getArticles();
   }, [qLocale, qCategory, qKeyword]);  //국가, 카테고리, 키워드 변경 시 리렌더링
